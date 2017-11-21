e3f89ad1898eb6a4952bf285ad0d9eb7f5957ff8
arash katchooi <akatchooi@pnimedia.com>
2017-11-21 15:59:59 -0800

    filtered list

--- routes/ExtraLinks/GetExtraLinks.js
+++ routes/ExtraLinks/GetExtraLinks.js
@@ -14,13 +14,18 @@ import { title, html } from './GetExtraLinks.md';
 
 class AboutPage extends React.Component {
 
+  myArray = [1,2,3];
+
   componentDidMount() {
     document.title = title;
   }
+  
 
   render() {
     return (
       <Layout className={s.content}>
+        <Counter/>
+        <FilteredList/>
         <Hello name="World" />,
         <h1>{title}</h1>
         <div dangerouslySetInnerHTML={{ __html: html }} />
@@ -30,11 +35,88 @@ class AboutPage extends React.Component {
 
 }
 
+export default AboutPage;
+
+
+///////
+
 class Hello extends React.Component {
   render() {
     return <div className={s.main}>Hello {this.props.name}</div>;
   }
 }
 
-export default AboutPage;
+var Counter = React.createClass({
+  incrementCount: function(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  getInitialState: function(){
+     return {
+       count: 0
+     }
+  },
+  render: function(){
+    return (
+      <div class="my-component">
+        <h1>Count: {this.state.count}</h1>
+        <button type="button" onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+});
 
+
+// reference : data flow https://scotch.io/tutorials/learning-react-getting-started-and-concepts
+
+var FilteredList = React.createClass({
+  filterList: function(event){
+    var updatedList = this.state.initialItems;
+    updatedList = updatedList.filter(function(item){
+      return item.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({items: updatedList});
+  },
+  getInitialState: function(){
+     return {
+       initialItems: [
+         "Apples",
+         "Broccoli",
+         "Chicken",
+         "Duck",
+         "Eggs",
+         "Fish",
+         "Granola",
+         "Hash Browns"
+       ],
+       items: []
+     }
+  },
+  componentWillMount: function(){
+    this.setState({items: this.state.initialItems})
+  },
+  render: function(){
+    return (
+      <div className="filter-list">
+        <input type="text" placeholder="Search" onChange={this.filterList}/>
+      <List items={this.state.items}/>
+      </div>
+    );
+  }
+});
+
+var List = React.createClass({
+  render: function(){
+    return (
+      <ul>
+      {
+        this.props.items.map(function(item) {
+          return <li key={item}>{item}</li>
+        })
+       }
+      </ul>
+    )  
+  }
+});
